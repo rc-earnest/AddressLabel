4d60f01cc488945c860a2e1e8d9661c1df946ff4
rc-earnest <rc.earnest04@gmail.com>
2025-03-15 21:05:05 -0600

    Added comments to the code

--- AddressLabel/AddressLabelForm.vb
+++ AddressLabel/AddressLabelForm.vb
@@ -6,8 +6,13 @@
 Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 
 Public Class AddressLabelForm
-
+    ''' <summary>
+    ''' Activates defaults and tool tips for each item in form.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub AddressLabelorm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'makes a tool tip for each item on the form
         ToolTip1.SetToolTip(FirstNameTextBox, "Enter first name")
         ToolTip1.SetToolTip(LastNameTextBox, "Enter last name")
         ToolTip1.SetToolTip(StreetAddressTextBox, "Enter street address")
@@ -19,6 +24,9 @@ Public Class AddressLabelForm
         ToolTip1.SetToolTip(ExitButton, "Exit the application")
         SetDefaults()
     End Sub
+    ''' <summary>
+    ''' Sets the defaults for each item on the form.
+    ''' </summary>
     Sub SetDefaults()
         FirstNameTextBox.Focus()
         FirstNameTextBox.Text = ""
@@ -29,17 +37,30 @@ Public Class AddressLabelForm
         ZipTextBox.Text = ""
         DisplayLabel.Text = ""
     End Sub
-
+    ''' <summary>
+    ''' Once the "Display" button is clicked it formats the items that are in the text
+    ''' boxs to how you would see them on an address label, and then displays it in the output address box.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayButton.Click
         Dim fullName As String = $"{FirstNameTextBox.Text} {LastNameTextBox.Text}"
         Dim formattedAddress As String = $"{fullName}{vbCrLf}{StreetAddressTextBox.Text}{vbCrLf}{CityTextBox.Text}, {StateTextBox.Text} {ZipTextBox.Text}"
         DisplayLabel.Text = formattedAddress
     End Sub
-
+    ''' <summary>
+    ''' "Clears" the text boxes once the clear button is clicked.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
         SetDefaults()
     End Sub
-
+    ''' <summary>
+    ''' Exits the form once the exit button was pressed.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
